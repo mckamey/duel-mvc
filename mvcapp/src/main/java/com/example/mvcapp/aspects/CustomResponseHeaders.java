a88c805e8b4165c71b33d52660405289359904ea
mckamey <devnull@localhost>
2011-09-18 11:24:25 -0700

    adding example of how to set custom response headers

---
+++ mvcapp/src/main/java/com/example/mvcapp/aspects/CustomResponseHeaders.java
@@ -0,0 +1,29 @@
+package com.example.mvcapp.aspects;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.duelengine.duel.mvc.ResultFilter;
+import org.duelengine.duel.mvc.ResultFilterContext;
+
+import com.google.inject.Inject;
+
+/**
+ * Example filter which injects custom response headers
+ */
+public class CustomResponseHeaders implements ResultFilter {
+
+	private final HttpServletResponse response;
+
+	@Inject
+	public CustomResponseHeaders(HttpServletResponse response) {
+		this.response = response;
+	}
+
+	@Override
+	public void onResultRendering(ResultFilterContext context) {
+		response.addHeader("X-UA-Compatible", "IE=edge,chrome=1");
+	}
+
+	@Override
+	public void onResultRendered(ResultFilterContext context) {}
+}
--- mvcapp/src/main/java/com/example/mvcapp/controllers/BaseController.java
+++ mvcapp/src/main/java/com/example/mvcapp/controllers/BaseController.java
@@ -3,12 +3,13 @@ package com.example.mvcapp.controllers;
 import org.duelengine.duel.mvc.Apply;
 import org.duelengine.duel.mvc.DuelController;
 
+import com.example.mvcapp.aspects.CustomResponseHeaders;
 import com.example.mvcapp.aspects.LatencyTimer;
 
 /**
  * Base for all controllers in app
  */
-@Apply({ LatencyTimer.class })
+@Apply({ CustomResponseHeaders.class, LatencyTimer.class })
 public abstract class BaseController extends DuelController {
 
 }
