02babf9d6c30b76ddec3fc43f6479ef30140e9ad
mckamey <devnull@localhost>
2011-09-18 11:58:21 -0700

    adding mvc lib into deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,9 @@
 
 clear;clear
 
+cd duel-mvc
+mvn clean deploy -U -DperformRelease=true -Dgpg.keyname=EE82F9AB
+cd ..
+
 cd duel-mvc-archetype
 mvn clean deploy -U -DperformRelease=true -Dgpg.keyname=EE82F9AB
