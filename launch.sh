955d62e2192aef81402fd55ef4001448fa58a22c
mckamey <devnull@localhost>
2013-03-30 10:56:01 -0700

    tweaking script

--- launch.sh
+++ launch.sh
@@ -8,7 +8,6 @@ java $JREBEL_OPTS \
 	-Djava.awt.headless=true \
 	-Drebel.jersey_plugin=true \
 	-jar bootstrap/target/bootstrap.jar \
-	-war mvcapp/target/mvcapp/ \
-	-c / \
+	-war /=mvcapp/target/mvcapp/ \
 	-p 8080 \
 	--tomcat
