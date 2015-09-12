72a68742dae4e051f8e928d637d718078a57a56f
mckamey <devnull@localhost>
2015-09-12 12:17:23 -0700

    Updating build settings.

--- launch.sh
+++ launch.sh
@@ -4,10 +4,9 @@ clear;
 
 # ensure mvcapp is built, then launch with bootstrap
 mvn clean package && \
-java $JREBEL_OPTS \
+java -Xms256m -Xmx1g \
 	-Djava.awt.headless=true \
-	-Drebel.jersey_plugin=true \
-	-jar bootstrap/target/bootstrap.jar \
-	-war /=mvcapp/target/mvcapp/ \
+	-jar 'bootstrap/target/bootstrap.jar' \
+	-war 'mvcapp/target/mvcapp/' \
 	-p 8080 \
 	--tomcat
